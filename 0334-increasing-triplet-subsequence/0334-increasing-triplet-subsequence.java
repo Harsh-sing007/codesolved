eceb946d4929ee36857716283147000ab503ccf6
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2025-07-28 22:16:04 +0530

    Time: 2 ms (96.47%), Space: 133.8 MB (74.52%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num; // smallest so far
+            } else if (num <= second) {
+                second = num; // second smallest
+            } else {
+                // num > first and num > second => increasing triplet found
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
