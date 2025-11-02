e57a53a81af4c3348660e0839e99cb33bccacbd7
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2025-11-02 22:56:46 +0530

    Time: 70 ms (50.46%), Space: 173.2 MB (63.3%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -6,33 +6,27 @@ public:
     const int WALL = 3;
 
     void markguarded(int row, int col, vector<vector<int>>& grid) {
-        // Traverse upwards
+
         for (int r = row - 1; r >= 0; r--) {
             if (grid[r][col] == WALL || grid[r][col] == GUARD) break;
             grid[r][col] = GUARDED;
         }
-        // Traverse downwards
         for (int r = row + 1; r < grid.size(); r++) {
             if (grid[r][col] == WALL || grid[r][col] == GUARD) break;
             grid[r][col] = GUARDED;
         }
-        // Traverse leftwards
         for (int c = col - 1; c >= 0; c--) {
             if (grid[row][c] == WALL || grid[row][c] == GUARD) break;
             grid[row][c] = GUARDED;
         }
-        // Traverse rightwards
         for (int c = col + 1; c < grid[row].size(); c++) {
             if (grid[row][c] == WALL || grid[row][c] == GUARD) break;
             grid[row][c] = GUARDED;
         }
     }
-
     int countUnguarded(int m, int n, vector<vector<int>>& guards,
                        vector<vector<int>>& walls) {
         vector<vector<int>> grid(m, vector<int>(n, UNGUARDED));
-
-        // Mark guards' positions
         for (const auto& guard : guards) {
             grid[guard[0]][guard[1]] = GUARD;
         }
