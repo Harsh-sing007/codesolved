7866d0cf4963ece0e45ea46c0503c02f247ca970
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2025-12-04 23:08:47 +0530

    Time: 11 ms (65.8%), Space: 19.3 MB (50.72%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    int countCollisions(string directions) {
-        int n = directions.size();
-        int l = 0, r = n - 1;
-
-        while (l < n && directions[l] == 'L') {
-            l++;
-        }
-
-        while (r >= l && directions[r] == 'R') {
-            r--;
-        }
-
-        int res = 0;
-        for (int i = l; i <= r; i++) {
-            if (directions[i] != 'S') {
-                res++;
+    int countCollisions(string D) {
+        int n=D.size();
+        if (n==1) return 0;
+        int l=0, r=n-1;
+        while (D[l]=='L') l++;
+        while (D[r]=='R') r--;
+        if (l>=r) return 0;
+        int col=0;
+    
+        for( ; l<=r; l++){
+            while(D[l]=='R'){
+                l++;
+                col++;
             }
+            if (D[l]=='L') col++;
         }
-        return res;
+        return col;      
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
