37cf8cc74f6766b00ceee0eff34a141b56a2ad81
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2025-12-29 22:52:30 +0530

    Time: 107 ms (87.57%), Space: 23.1 MB (29.73%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     unordered_map<string, vector<char>> mp;
+    unordered_map<string, bool> memo;
 
-    bool dfs(string curr, string next, int idx) {
+    bool solve(string curr) {
         if (curr.size() == 1) return true;
 
-        if (idx == curr.size() - 1)
-            return dfs(next, "", 0);
+        if (memo.count(curr)) return memo[curr];
 
-        string key = curr.substr(idx, 2);
-        if (mp.find(key) == mp.end()) return false;
+        int n = curr.size();
+        vector<string> choices(n - 1);
 
-        for (char c : mp[key]) {
-            if (dfs(curr, next + c, idx + 1))
-                return true;
+        for (int i = 0; i < n - 1; i++) {
+            string key = curr.substr(i, 2);
+            if (!mp.count(key)) return memo[curr] = false;
+
+            for (char c : mp[key])
+                choices[i].push_back(c);
         }
-        return false;
+
+        function<bool(int, string&)> dfs = [&](int idx, string &next) {
+            if (idx == choices.size())
+                return solve(next);
+
+            for (char c : choices[idx]) {
+                next.push_back(c);
+                if (dfs(idx + 1, next)) return true;
+                next.pop_back();
+            }
+            return false;
+        };
+
+        string next = "";
+        return memo[curr] = dfs(0, next);
     }
 
     bool pyramidTransition(string bottom, vector<string>& allowed) {
         for (auto &s : allowed) {
-            mp[s.substr(0,2)].push_back(s[2]);
+            mp[s.substr(0, 2)].push_back(s[2]);
         }
-        return dfs(bottom, "", 0);
+        return solve(bottom);
     }
 };
