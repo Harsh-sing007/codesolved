bba1c071345fca58a0e858c5b01b11cfd4797abd
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2025-12-29 22:55:43 +0530

    Time: 103 ms (87.57%), Space: 23.2 MB (29.73%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -2,43 +2,36 @@ class Solution {
 public:
     unordered_map<string, vector<char>> mp;
     unordered_map<string, bool> memo;
-
-    bool solve(string curr) {
+    bool dfs(string curr) {
         if (curr.size() == 1) return true;
-
         if (memo.count(curr)) return memo[curr];
-
-        int n = curr.size();
-        vector<string> choices(n - 1);
-
-        for (int i = 0; i < n - 1; i++) {
+        vector<string> choices(curr.size() - 1);
+        for (int i = 0; i < curr.size() - 1; i++) {
             string key = curr.substr(i, 2);
-            if (!mp.count(key)) return memo[curr] = false;
+            if (!mp.count(key))
+                return memo[curr] = false;
 
             for (char c : mp[key])
                 choices[i].push_back(c);
         }
-
-        function<bool(int, string&)> dfs = [&](int idx, string &next) {
+        function<bool(int, string&)> build = [&](int idx, string &next) {
             if (idx == choices.size())
-                return solve(next);
+                return dfs(next);
 
             for (char c : choices[idx]) {
                 next.push_back(c);
-                if (dfs(idx + 1, next)) return true;
+                if (build(idx + 1, next)) return true;
                 next.pop_back();
             }
             return false;
         };
-
         string next = "";
-        return memo[curr] = dfs(0, next);
+        return memo[curr] = build(0, next);
     }
-
     bool pyramidTransition(string bottom, vector<string>& allowed) {
-        for (auto &s : allowed) {
+        for (auto &s : allowed)
             mp[s.substr(0, 2)].push_back(s[2]);
-        }
-        return solve(bottom);
+
+        return dfs(bottom);
     }
 };
