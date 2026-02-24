ec35117609d848d6d44d1871193b94e44bda82be
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-02-24 23:31:57 +0530

    Time: 0 ms (100%), Space: 17.4 MB (34.02%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
+    }
+
+    int dfs(TreeNode* node, int current) {
+        if (!node) return 0;
+
+        current = current * 2 + node->val;
+        if (!node->left && !node->right)
+            return current;
+
+        return dfs(node->left, current) + dfs(node->right, current);
+    }
+};
