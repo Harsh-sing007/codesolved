93b78c75d0977d038ba5b498705bdd13924dfb4b
Harsh singh <149600919+Harsh-sing007@users.noreply.github.com>
2026-02-25 23:39:35 +0530

    Time: 3 ms (47.07%), Space: 14.3 MB (58.88%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -4,30 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-
-    int countBits(int n) {
-        int count = 0;
-        while(n>0){
-            if(n%2==1)
-                count++;
-            n=n/2;
-        }
-        return count;
+    
+    static bool compare(int a, int b) {
+        
+        int countA = __builtin_popcount(a);
+        int countB = __builtin_popcount(b);
+        
+        if(countA == countB)
+            return a < b;
+            
+        return countA < countB;
     }
-
+    
     vector<int> sortByBits(vector<int>& arr) {
-
-        sort(arr.begin(), arr.end(), [&](int a, int b){
-
-            int c1 = countBits(a);
-            int c2 = countBits(b);
-
-            if(c1==c2)
-                return a<b;
-
-            return c1<c2;
-        });
-
+        
+        sort(arr.begin(), arr.end(), compare);
+        
         return arr;
     }
 };
